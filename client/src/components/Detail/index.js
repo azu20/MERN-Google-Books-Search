fb8a673cc57db2ab6f38e3fa29e6b7707e3095da
Azucena Herrera <azucenaphx@gmail.com>
2020-08-03 18:55:14 -0700

    workign on error

--- client/src/components/Detail/index.js
+++ client/src/components/Detail/index.js
@@ -3,7 +3,7 @@ import React from "react";
  
 export function DetailImage({children} ) {
     return (       
-            <img style="width: 30%" src={children} className="card-img" alt="🤓" />  
+            <img src={children} className="card-img" alt="🤓" />  
     );
   }
 
--- client/src/pages/MyList.js
+++ client/src/pages/MyList.js
@@ -5,7 +5,6 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, FormBtn } from "../components/Form";
 import { DetailBody, DetailImage } from "../components/Detail/";
 
 
